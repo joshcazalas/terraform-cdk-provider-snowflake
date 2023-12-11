1f38fd85a29557dc6c434825e503bce1315c377c
joshcazalas <joshuacazalas@gmail.com>
2023-12-11 12:32:37 +0000

    Update Triggered by GitHub Actions Workflow for New Release v0.79.0

--- src/snowflake_resources/snowflake_provider.ts
+++ src/snowflake_resources/snowflake_provider.ts
@@ -33,7 +33,7 @@ export class SnowflakeProvider extends TerraformProvider {
             terraformResourceType: "snowflake",
             terraformGeneratorMetadata: {
                 providerName: "snowflake",
-                providerVersionConstraint: "0.78.0"
+                providerVersionConstraint: "0.79.0"
             },
             terraformProviderSource: "terraform-cdk-provider-snowflake"
         })
